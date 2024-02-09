35a0c13385cb5aead0f019d0f810f7a689cf81a5
Jan Range <range.jan@web.de>
2024-02-09 12:49:21 +0100

    filter files if `filenames` are provided

--- easyDataverse/utils.py
+++ easyDataverse/utils.py
@@ -19,11 +19,14 @@ def download_files(
     dataset,
     files_list: List[Dict],
     filedir: str,
+    filenames: List[str],
 ) -> None:
     """Downloads and adds all files given in the dataset to the Dataset-Object"""
 
     if len(files_list) == 0:
         return
+
+    files_list = _filter_files(files_list, filenames)
 
     if filedir is not None:
         # Set up the progress bar
@@ -36,11 +39,12 @@ def download_files(
         filename = file["dataFile"]["filename"]
         file_pid = file["dataFile"]["id"]
         directory_label = file.get("directoryLabel", "")
+        dv_path = os.path.join(directory_label, filename)
 
         if filedir:
-            local_path = os.path.join(filedir, directory_label, filename)
+            local_path = os.path.join(filedir, dv_path)
         else:
-            local_path = os.path.join(filedir, filename)
+            local_path = dv_path
 
         response = data_api.get_datafile(file_pid)
         response.raise_for_status()
@@ -56,3 +60,31 @@ def download_files(
         )
 
         dataset.files.append(datafile)
+
+
+def _filter_files(files: List[Dict], filenames: List[str]) -> List[Dict]:
+    """Filters a list of files by filenames
+
+    Args:
+        files (List[Dict]): The list of files to filter.
+        filenames (List[str]): The list of filenames to filter by.
+
+    Returns:
+        List[Dict]: The filtered list of files.
+    """
+
+    if len(filenames) == 0:
+        return files
+
+    to_download = []
+
+    for file in files:
+        dv_path = os.path.join(
+            file.get("directoryLabel", ""),
+            file["dataFile"]["filename"],
+        )
+
+        if dv_path in filenames:
+            to_download.append(file)
+
+    return to_download
