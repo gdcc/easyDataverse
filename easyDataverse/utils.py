a778987b64352714171b9e143fecb41bb2a0ed40
Jan Range <range.jan@web.de>
2024-02-12 17:30:41 +0100

    async file downloads

--- easyDataverse/utils.py
+++ easyDataverse/utils.py
@@ -1,12 +1,18 @@
+import asyncio
 import os
-import sys
 from typing import Dict, List
+import aiofiles
+import aiohttp
+import rich
 
-import tqdm
+from rich.progress import Progress, TaskID
+from rich.panel import Panel
 from pyDataverse.api import DataAccessApi
 import yaml
 
 from dvuploader import File
+
+CHUNK_SIZE = 10 * 1024**2  # 10 MB
 
 
 class YAMLDumper(yaml.Dumper):
@@ -14,52 +20,139 @@ class YAMLDumper(yaml.Dumper):
         return super(YAMLDumper, self).increase_indent(flow, False)
 
 
-def download_files(
+async def download_files(
     data_api: DataAccessApi,
-    dataset,
     files_list: List[Dict],
     filedir: str,
     filenames: List[str],
-) -> None:
+) -> List[File]:
     """Downloads and adds all files given in the dataset to the Dataset-Object"""
 
-    if len(files_list) == 0:
-        return
-
     files_list = _filter_files(files_list, filenames)
+    progress, task_ids = setup_progress_bars(files=files_list)
 
-    if filedir is not None:
-        # Set up the progress bar
-        files_list = tqdm.tqdm(files_list, file=sys.stdout)  # type: ignore
-        files_list.set_description(f"Downloading data files")  # type: ignore
+    if len(files_list) == 0:
+        return []
 
-    for file in files_list:
+    with progress:
 
-        # Get file metdata
+        rich.print("\n[bold]Downloading files[/bold]\n")
+
+        tasks = [
+            _download_file(
+                file=file,
+                filedir=filedir,
+                data_api=data_api,
+                progress=progress,
+                task_id=task_id,
+            )
+            for file, task_id in zip(files_list, task_ids)
+        ]
+
+        files = await asyncio.gather(*tasks)
+
+    rich.print("\n[bold]✅ Done [/bold]\n\n")
+
+    return files
+
+
+def setup_progress_bars(
+    files: List[Dict],
+):
+    """
+    Sets up progress bars for each file.
+
+    Returns:
+        A list of progress bars, one for each file.
+    """
+
+    tasks = []
+    progress = Progress()
+
+    for file in files:
+
         filename = file["dataFile"]["filename"]
-        file_pid = file["dataFile"]["id"]
+        filesize = file["dataFile"]["filesize"]
         directory_label = file.get("directoryLabel", "")
-        dv_path = os.path.join(directory_label, filename)
+        filepath = os.path.join(directory_label, filename)
 
-        if filedir:
-            local_path = os.path.join(filedir, dv_path)
-        else:
-            local_path = dv_path
-
-        response = data_api.get_datafile(file_pid)
-        response.raise_for_status()
-
-        os.makedirs(os.path.dirname(local_path), exist_ok=True)
-        with open(local_path, "wb") as f:
-            f.write(response.content)
-
-        datafile = File(
-            filepath=local_path,
-            file_id=str(file_pid),
-            **file,
+        tasks.append(
+            setup_pbar(
+                fpath=filepath,
+                filesize=filesize,
+                progress=progress,
+            )
         )
 
-        dataset.files.append(datafile)
+    return progress, tasks
+
+
+def setup_pbar(
+    fpath: str,
+    filesize: int,
+    progress: Progress,
+) -> int:
+    """
+    Set up a progress bar for a file.
+
+    Args:
+        fpath (str): The path to the file.
+        progress (Progress): The progress bar object.
+
+    Returns:
+        int: The task ID of the progress bar.
+    """
+
+    return progress.add_task(
+        f"[pink]  {fpath}",
+        total=filesize,
+    )
+
+
+async def _download_file(
+    file: Dict,
+    filedir: str,
+    data_api: DataAccessApi,
+    progress: Progress,
+    task_id: TaskID,
+):
+
+    # Get file metdata
+    filename = file["dataFile"]["filename"]
+    file_id = file["dataFile"]["id"]
+    directory_label = file.get("directoryLabel", "")
+    dv_path = os.path.join(directory_label, filename)
+
+    if filedir:
+        local_path = os.path.join(filedir, dv_path)
+    else:
+        local_path = dv_path
+
+    if data_api.api_token:
+        headers = {"X-Dataverse-key": data_api.api_token}
+    else:
+        headers = {}
+
+    url = f"{data_api.base_url.rstrip('/')}/api/access/datafile/{file_id}"
+
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            os.makedirs(os.path.dirname(local_path), exist_ok=True)
+
+            async with aiofiles.open(local_path, "wb") as f:
+                while True:
+                    chunk = await response.content.read(CHUNK_SIZE)
+                    progress.advance(task_id, advance=len(chunk))
+                    if not chunk:
+                        break
+                    await f.write(chunk)
+
+    return File(
+        filepath=local_path,
+        file_id=str(file_id),
+        **file,
+    )
 
 
 def _filter_files(files: List[Dict], filenames: List[str]) -> List[Dict]:
@@ -72,6 +165,9 @@ def _filter_files(files: List[Dict], filenames: List[str]) -> List[Dict]:
     Returns:
         List[Dict]: The filtered list of files.
     """
+
+    # Sort files by size
+    files = sorted(files, key=lambda x: int(x["dataFile"]["filesize"]))
 
     if len(filenames) == 0:
         return files
