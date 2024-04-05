6650b7eadbef14073a957f4d02cb4c248c01dd0e
Jan Range <range.jan@web.de>
2024-04-05 13:50:03 +0200

    add test for template export

---
+++ tests/unit/test_base.py
@@ -0,0 +1,40 @@
+import pytest
+
+from typing import List, Optional
+from pydantic import Field
+from easyDataverse.base import DataverseBase
+
+
+class TestBase:
+
+    @pytest.mark.unit
+    def test_template(self):
+
+        # Arrange
+        class Child(DataverseBase):
+            bar: Optional[str] = Field(
+                default=None,
+                alias="Foo",
+            )
+
+        class Test(DataverseBase):
+
+            foo: Optional[str] = Field(
+                default=None,
+                alias="Foo",
+            )
+            nested: List[Child] = Field(
+                default_factory=list,
+                alias="Nested",
+            )
+
+        # Act
+        example = Test._construct_example_ds(Test)
+
+        # Assert
+        expected = {
+            "Foo": "Enter data of type str",
+            "Nested": [{"Foo": "Enter data of type str"}],
+        }
+
+        assert example == expected, "Example data is not as expected"
