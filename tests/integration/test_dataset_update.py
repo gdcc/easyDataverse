9ee8c877f8763f79ca18c57f56046af13abfb8f5
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-09-02 11:51:03 +0200

    Add support for updating dataset license
    Introduces logic to update a dataset's license, supporting both predefined and custom licenses via JSON-LD metadata. Adds integration tests for custom and predefined license updates, and a fixture for minimal custom license upload.

--- tests/integration/test_dataset_update.py
+++ tests/integration/test_dataset_update.py
@@ -2,8 +2,10 @@ from typing import Dict
 
 import pytest
 import httpx
+import rich
 
 from easyDataverse import Dataverse
+from easyDataverse.license import CustomLicense, License
 
 
 class TestDatasetUpdate:
@@ -61,6 +63,121 @@ class TestDatasetUpdate:
         # Assert
         assert title_field["value"] == "Title has changed", (
             "The updated dataset title does not match the expected title."
+        )
+
+    @pytest.mark.integration
+    def test_custom_license_update(
+        self,
+        credentials,
+        minimal_upload_custom_license,
+    ):
+        # Arrange
+        base_url, api_token = credentials
+        url = f"{base_url}/api/dataverses/root/datasets"
+        response = httpx.post(
+            url=url,
+            json=minimal_upload_custom_license,
+            headers={
+                "X-Dataverse-key": api_token,
+                "Content-Type": "application/json",
+            },
+        )
+
+        response.raise_for_status()
+        pid = response.json()["data"]["persistentId"]
+
+        # Act
+        dataverse = Dataverse(
+            server_url=base_url,
+            api_token=api_token,
+        )
+
+        # Fetch the dataset and update the title
+        dataset = dataverse.load_dataset(pid)
+        dataset.license = CustomLicense(
+            termsOfUse="CHANGED",
+            confidentialityDeclaration="CHANGED",
+            specialPermissions="CHANGED",
+            restrictions="CHANGED",
+            citationRequirements="CHANGED",
+            conditions="CHANGED",
+            depositorRequirements="CHANGED",
+            disclaimer="CHANGED",
+        )
+
+        dataset.update()
+
+        # Re-fetch the dataset
+        refetched_dataset = dataverse.load_dataset(pid)
+
+        # Assert
+        assert isinstance(refetched_dataset.license, CustomLicense)
+        assert refetched_dataset.license.terms_of_use == "CHANGED"
+        assert refetched_dataset.license.confidentiality_declaration == "CHANGED"
+        assert refetched_dataset.license.special_permissions == "CHANGED"
+        assert refetched_dataset.license.restrictions == "CHANGED"
+        assert refetched_dataset.license.citation_requirements == "CHANGED"
+        assert refetched_dataset.license.conditions == "CHANGED"
+        assert refetched_dataset.license.depositor_requirements == "CHANGED"
+        assert refetched_dataset.license.disclaimer == "CHANGED"
+
+        assert dataset.dataverse_dict() == refetched_dataset.dataverse_dict(), (
+            "Dataset contents are not the same"
+        )
+
+    @pytest.mark.integration
+    def test_custom_license_update_with_predefined_license(
+        self,
+        credentials,
+        minimal_upload,
+    ):
+        # Arrange
+        base_url, api_token = credentials
+        url = f"{base_url}/api/dataverses/root/datasets"
+        response = httpx.post(
+            url=url,
+            json=minimal_upload,
+            headers={
+                "X-Dataverse-key": api_token,
+                "Content-Type": "application/json",
+            },
+        )
+
+        response.raise_for_status()
+        pid = response.json()["data"]["persistentId"]
+
+        # Act
+        dataverse = Dataverse(
+            server_url=base_url,
+            api_token=api_token,
+        )
+
+        # Fetch the dataset and update the title
+        dataset = dataverse.load_dataset(pid)
+        assert isinstance(dataset.license, License), (
+            "Dataset license is not a predefined license"
+        )
+
+        # Update the license to a different predefined license
+        expected_license = next(
+            license
+            for license in dataverse.licenses.values()
+            if license.name != dataset.license.name
+        )
+        dataset.license = expected_license
+
+        dataset.update()
+
+        # Re-fetch the dataset
+        refetched_dataset = dataverse.load_dataset(pid)
+
+        # Assert
+        assert refetched_dataset.license == expected_license, (
+            "Dataset license is not the expected license"
+        )
+
+        assert dataset.dataverse_dict() == refetched_dataset.dataverse_dict(), (
+            "Dataset contents are not the same"
         )
 
     @staticmethod
